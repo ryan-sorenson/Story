426c7e54153e9328b0f672435d97a9605c172ca2
Student <student@bluc02mg0l9fh51>
2015-12-13 19:44:51 -0600

    Initial Commit

---
+++ The actual actual final ProjectTests/The_actual_actual_final_ProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_actual_actual_final_ProjectTests.swift
+//  The actual actual final ProjectTests
+//
+//  Created by Student on 12/13/15.
+//  Copyright © 2015 Student. All rights reserved.
+//
+
+import XCTest
+@testable import The_actual_actual_final_Project
+
+class The_actual_actual_final_ProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
